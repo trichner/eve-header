535d6559b2d3ec27639118d06052b971b9a5a47d
Thomas Richner <thomasrichner+github@gmail.com>
2015-04-11 01:36:32 +0200

    initial commit

---
+++ eve-header.js
@@ -0,0 +1,49 @@
+var eve_headers = {
+  'eve_trusted' : 'trusted',
+  'eve_serverip' : 'serverIp',
+  'eve_charname' : 'char.name',
+  'eve_charid' : 'char.id',
+  'eve_corpname' : 'corp.name',
+  'eve_corpid' : 'corp.id',
+  'eve_alliancename' : 'alliance.name',
+  'eve_allianceid' : 'alliance.id',
+  'eve_regionname' : 'region.name',
+  'eve_constellationname' : 'constellation.name',
+  'eve_solarsystemname' : 'solarsystem.name',
+  'eve_stationname' : 'station.name',
+  'eve_stationid' : 'station.id',
+  'eve_corprole' : 'corp.role',
+  'eve_solarsystemid' : 'solarsystem.id',
+  'eve_warfactionid' : 'warfaction.id',
+  'eve_shipid' : 'ship.id',
+  'eve_shipname' : 'ship.name',
+  'eve_shiptypeid' : 'ship.type.id',
+  'eve_shiptypename' : 'ship.type.name'
+}
+
+/* GET users listing. */
+
+module.exports = function(req, res, next) {
+  for (var key in eve_headers) {
+    if (eve_headers.hasOwnProperty(key)) {
+      setPath(req,eve_headers[key],req.get(key));
+    }
+  }
+  next();
+};
+
+function setPath(obj,path,value){
+  path = path.split('.');
+  if(path.length==0){
+    return;
+  }
+  var node = obj;
+  var branch = path.shift();
+  while(path.length>0){
+    node[branch] = node[branch] || {};
+    node = node[branch];
+    branch = path.shift();
+  }
+  node[branch] = value;
+  return obj;
+}
